f79a8a0732d08b8a482a0fb6161297514ab8d8a4
ws0h3ll <45339471+ws0h3ll@users.noreply.github.com>
2019-07-21 02:04:45 +0100

    Create strings.java

---
+++ strings.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.lang.*;
+class main{
+    public static void main(String[] args){
+        String s = "This is a test";
+        System.out.println("String s = " + s);
+        String news = new String("This is my second string");
+        System.out.println("String 2 = " + news);
+    }
+}
