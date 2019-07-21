c56bacb02ce2a2e4ede54264d5b014b9f18b753b
ws0h3ll <45339471+ws0h3ll@users.noreply.github.com>
2019-07-21 02:13:59 +0100

    Create strcases.java

---
+++ strcases.java
@@ -0,0 +1,8 @@
+class main{
+    public static void main(String[] arg){
+        String mystr = "This is a string";
+        System.out.println("You String in upper = " + mystr.toUpperCase());
+        System.out.println("You String in Lower = " + mystr.toLowerCase());
+    }
+    
+}
